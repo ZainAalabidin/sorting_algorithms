39343aca95f761903f23134f6106c5e638d752b3
ZainAalabidin <zynly9303@gmail.com>
2024-02-21 04:18:30 +0200

    x

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array lements from minmun to maxmum
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t bs_iter = 0;
+    size_t adj_iter = 0;
+
+    if(size < 2)
+        return;
+
+    for(bs_iter=0; bs_iter < size-1; bs_iter++)
+    {
+        for(adj_iter=0; adj_iter < size-1-bs_iter; adj_iter++)
+        {
+            if(array[adj_iter] > array[adj_iter+1])
+            {
+                swap(&array[adj_iter], &array[adj_iter+1]);
+                print_array(array, size);
+            }
+        }
+    }
+}
+/**
+ * swap - function swap two number
+ * @num1: first number
+ * @num2: second numder
+ */
+void swap(int *num1, int *num2)
+{
+    size_t temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function sort a doubly linked list
+ * @list: Dobule linked list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *first;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	first = (*list)->next;
+	while(first)
+	{
+		while ((first->prev) && (first->prev->n > first->n))
+		{
+			first = swap_node(first, list);
+			print_list(*list);
+		}
+		first = first->next;
+	}
+}
+/**
+ *swap_node - swap a node
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
+ */
+listint_t *swap_node(listint_t *first, listint_t **list)
+{
+	listint_t *prevn = first->prev, *currn = first;
+
+	prevn->next = currn->next;
+	if (currn->next)
+		currn->next->prev = prevn;
+	currn->next = prevn;
+	currn->prev = prevn->prev;
+	prevn->prev = currn;
+	if (currn->prev)
+		currn->prev->next = currn;
+	else
+		*list = currn;
+	return (currn);
+}
